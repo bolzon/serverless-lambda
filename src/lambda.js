5ae4146ee1cf6898228900afa102706b833cc89a
Alexandre Bolzon <blzn@mail.ru>
2019-08-26 17:35:10 -0300

    Adding spaces between steps

--- src/lambda.js
+++ src/lambda.js
@@ -13,21 +13,28 @@ const reS3EncryptedPrefix = new RegExp(`^${S3_ENCRYPTED_PREFIX}`, 'i');
 exports.encryptS3File = async event => {
   const startTime = Date.now();
   const body = event.body || event;
+
   console.log(JSON.stringify({ timestamp: Date.now(), body }, null, 2));
+
   for (const record of body.Records || []) {
     const objectSize = record.s3.object.size;
     const objectKey = record.s3.object.key;
+
     if (reS3EncryptedPrefix.test(objectKey) || objectSize === 0) {
       console.log(`Skipping ${objectKey}`);
       continue; // skip this record
     }
+
     const bucketName = record.s3.bucket.name;
     const file = await Storage.getFile(bucketName, objectKey);
     const fileBuffer = await encrypt(file.content);
+
     const destObjectKey = `${S3_ENCRYPTED_PREFIX}${objectKey.replace(/.*\/([^/]+)$/, '$1')}`;
     console.log(`destObjectKey = ${destObjectKey}`);
+
     const url = await Storage.putFile(bucketName, destObjectKey, fileBuffer);
     console.log(`url = ${url}`);
+
     await Storage.deleteFile(bucketName, objectKey);
     console.log(`Total time: ${Date.now() - startTime}ms`);
   }
