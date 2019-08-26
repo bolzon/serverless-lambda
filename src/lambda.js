50b689054bca8446b0d2cb8c57b898a939979d71
Alexandre Bolzon <blzn@mail.ru>
2019-08-26 15:52:36 -0300

    Removing save file lambda function + dependencies

--- src/lambda.js
+++ src/lambda.js
@@ -2,7 +2,6 @@
 
 const { encrypt } = require('./sec');
 const { Storage } = require('./storage');
-const { validate } = require('./body-validation');
 
 const S3_ENCRYPTED_PREFIX = process.env.S3_ENCRYPTED_DIR || 'encrypted/';
 const reS3EncryptedPrefix = new RegExp(`^${S3_ENCRYPTED_PREFIX}`, 'i');
@@ -30,44 +29,4 @@ exports.encryptS3File = async event => {
     console.log(`url = ${url}`);
     await Storage.deleteFile(bucketName, objectKey);
   }
-  return ok();
 };
-
-/**
- * This function is called when the API gateway
- * is called passing a new file to be stored into S3.
- */
-exports.saveS3File = async event => {
-  const body = event.body || event;
-  try {
-    await validate(body);
-  } catch (ex) {
-    return nok(ex.stack || ex.message || ex);
-  }
-  const { bucketName, fileName } = body;
-  const fileContent = Buffer.from(body.fileContent, 'base64');
-  const fileUrl = await Storage.putFile(bucketName, fileContent);
-  return ok({
-    bucketName,
-    fileName,
-    fileUrl
-  });
-};
-
-/**
- * Create OK response payload.
- */
-const ok = (body = null) => ({
-  statusCode: 200,
-  body: typeof body === 'string' ? body : JSON.stringify(body || {}),
-  headers: { 'content-type': 'application/json' }
-});
-
-/**
- * Create error response payload (bad request = 400).
- */
-const nok = error => ({
-  statusCode: 400,
-  body: JSON.stringify({ error }),
-  headers: { 'content-type': 'application/json' }
-});
